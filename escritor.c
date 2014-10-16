504d80adabad2542491ac3aabd9d03571c222a07
Pedro Saraiva <pedrovskyandre@hotmail.com>
2014-10-16 18:04:52 +0100

    escritor ex2

--- escritor.c
+++ escritor.c
@@ -9,6 +9,7 @@
 
 #include <stdio.h>
 #include <fcntl.h>
+#include <stdlib.h>
 #include "consts.h"
 
 int main(int argc, char *argv[]) {
@@ -29,18 +30,59 @@ int main(int argc, char *argv[]) {
   fileName[3] = "SO2014-3.txt";
   fileName[4] = "SO2014-4.txt";
 
-  int i, j;
-  for(i=0; i<TOTAL_WRITES; i++) {
-    int fileIndex = rand() % N_FILES; /*escolhemos o ficheiro aleatoriamente */
-    int strIndex = rand() % N_STRINGS; /*escolhemos a cadeia aleatoriamente */
+  pid_t *childPids = NULL;
+  pid_t p;
 
-    int file = open(fileName[fileIndex], O_CREAT | O_RDWR, 0644); /* abrimos o ficheiro ou criamos caso este não exista. caso o ficheiro exista o seu conteúdo é substituído*/
+int ii;
 
-    for(j=0; j<TOTAL_LINES; j++) {
-      write(file, str[strIndex], STR_LENGTH); /*escrevemos a cadeia de caracteres escolhida aleatoriamente 1024 vezes */
+  childPids = malloc(N_CHILDREN * sizeof(pid_t));
+
+  for (ii = 0; ii < N_CHILDREN; ++ii) {
+    if ((p = fork()) == 0) {
+ 
+      int i, j;
+      for(i=0; i<TOTAL_WRITES/N_CHILDREN; i++) {
+        int fileIndex = rand() % N_FILES; /*escolhemos o ficheiro aleatoriamente */
+        int strIndex = rand() % N_STRINGS; /*escolhemos a cadeia aleatoriamente */
+
+        int file = open(fileName[fileIndex], O_CREAT | O_WRONLY | O_TRUNC, 0644); /* abrimos o ficheiro ou criamos caso este não exista. caso o ficheiro exista o seu conteúdo é substituído*/
+
+        for(j=0; j<TOTAL_LINES; j++) {
+          write(file, str[strIndex], STR_LENGTH); /*escrevemos a cadeia de caracteres escolhida aleatoriamente 1024 vezes */
+        }
+
+        close(file);
+      }
+
+      exit(0);
     }
-
-    close(file);
+    else {
+      childPids[ii] = p;
+    }
   }
+
+  /* Wait for children to exit */
+  int stillWaiting;
+  do {
+    stillWaiting = 0;
+    for (ii = 0; ii < N_CHILDREN; ++ii) {
+      if (childPids[ii] > 0) {
+        if (waitpid(childPids[ii], NULL, WNOHANG) != 0) {
+          /* Child acabou */
+          childPids[ii] = 0;
+        }
+        else {
+          /* Still waiting on this child */
+          stillWaiting = 1;
+        }
+      }
+      /* Give up timeslice and prevent hard loop: this may not work on all flavors of Unix */
+      sleep(0);
+    }
+  } while (stillWaiting);
+
+  /* Cleanup */
+  free(childPids);
+
   return 0;
 }
