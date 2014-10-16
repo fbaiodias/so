88259e61b800b66719d3489997794efb8e3418c6
Pedro Saraiva <pedrovskyandre@hotmail.com>
2014-10-16 18:06:01 +0100

    leitor avaliacao

--- leitor.c
+++ leitor.c
@@ -4,7 +4,7 @@
  * Pedro Saraiva nº70848
  * Francisco Dias nº75328
  * 
- * Exercício 1
+ * Exercício 1 Avaliacao
  * */
 
 #include <stdio.h>
@@ -29,40 +29,45 @@ int main(int argc, char *argv[]) {
   
   size_t nbytes;
   
-  int fileIndex = rand() % N_FILES;
+  int fileIndex; // = rand() % N_FILES;
   
-  int file = open(fileName[fileIndex], O_RDONLY, 0644);
+  for(fileIndex=0; fileIndex<N_FILES; fileIndex++) {
+
+    int file = open(fileName[fileIndex], O_RDONLY, 0644);
   
-  timesRead = 0;
+    timesRead = 0;
   
-  nbytes = sizeof(buffer);
+    nbytes = sizeof(buffer);
   
-  /* não conseguimos abrir o ficheiro por isso paramos a execução */
+     /* não conseguimos abrir o ficheiro por isso paramos a execução */
   
-  if(file == -1)
-  {
+    if(file == -1)
+    {
 	  return -2;
-  }
+    }
   
-  read(file, buffer, nbytes); /* lemos a primeira linha para comparação */
-  strcpy(firstLine, buffer); /* guardamos a primeira linha */
+    read(file, buffer, nbytes); /* lemos a primeira linha para comparação */
+    strcpy(firstLine, buffer); /* guardamos a primeira linha */
   
-  for(i=0; i<TOTAL_LINES; i++) {
+    for(i=0; i<TOTAL_LINES; i++) {
 	  if(strncmp(firstLine, buffer, STR_LENGTH) == 0) /* comparação da primeira linha com cada uma das restantes */
 	  {
 		  timesRead++;
 	  }
 	  read(file, buffer, nbytes);
+    }
+  
+    close(file);
+  
+    if(timesRead == TOTAL_LINES) /* verifica que se existe a mesma cadeia de caracteres repetida 1024 e devolve 0 se true e -1 se false */
+    {
+	printf("%s is valid\n", fileName[fileIndex]);
+    }
+    else  {
+        printf("%s is NOT valid\n", fileName[fileIndex]);
+    }
   }
   
-  close(file);
-  
-  if(timesRead == TOTAL_LINES) /* verifica que se existe a mesma cadeia de caracteres repetida 1024 e devolve 0 se true e -1 se false */
-  {
-	return 0;
-  }
-  else{
-	return -1;
-  }
+  return 0;
 
 }
