9aeea55f980bfefd86afe6365a029fa3e651f400
Pedro Saraiva <pedrovskyandre@hotmail.com>
2014-10-30 18:27:43 +0000

    Update leitor.c

--- leitor.c
+++ leitor.c
@@ -4,65 +4,88 @@
  * Pedro Saraiva nº70848
  * Francisco Dias nº75328
  * 
- * Exercício 1
+ * Exercício 3
  * */
 
 #include <stdio.h>
 #include <fcntl.h>
-#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
 #include <string.h>
+#include <sys/file.h>
+
 #include "consts.h"
 
-int main(int argc, char *argv[]) {
+/*-------------------------------------------------------------------------------------
+| fixed strings
++-------------------------------------------------------------------------------------*/
 
-  fileName[0] = "SO2014-0.txt";
-  fileName[1] = "SO2014-1.txt";
-  fileName[2] = "SO2014-2.txt";
-  fileName[3] = "SO2014-3.txt";
-  fileName[4] = "SO2014-4.txt";
 
-  int i, timesRead;
-  
-  char buffer[STR_LENGTH];
-  
-  char firstLine[STR_LENGTH];
-  
-  size_t nbytes;
-  
-  int fileIndex = rand() % N_FILES;
-  
-  int file = open(fileName[fileIndex], O_RDONLY, 0644);
-  
-  timesRead = 0;
-  
-  nbytes = sizeof(buffer);
-  
-  /* não conseguimos abrir o ficheiro por isso paramos a execução */
-  
-  if(file == -1)
-  {
-	  return -2;
-  }
-  
-  read(file, buffer, nbytes); /* lemos a primeira linha para comparação */
-  strcpy(firstLine, buffer); /* guardamos a primeira linha */
-  
-  for(i=0; i<TOTAL_LINES; i++) {
-	  if(strncmp(firstLine, buffer, STR_LENGTH) == 0) /* comparação da primeira linha com cada uma das restantes */
-	  {
-		  timesRead++;
-	  }
-	  read(file, buffer, nbytes);
-  }
-  
-  close(file);
-  
-  if(timesRead == TOTAL_LINES) /* verifica que se existe a mesma cadeia de caracteres repetida 1024 e devolve 0 se true e -1 se false */
-  {
-	return 0;
-  }
-  else{
-	return -1;
-  }
+char* myfiles[N_FILES] = { "SO2014-0.txt", 
+			    "SO2014-1.txt",
+			    "SO2014-2.txt",
+			    "SO2014-3.txt",
+			    "SO2014-4.txt"};
 
+
+/*-------------------------------------------------------------------------------------
+| main
++-------------------------------------------------------------------------------------*/
+
+int main (int argc, char** argv) {
+  char  *file_to_open;
+  int   fd, lock;
+
+  /* Initialize the seed of random number generator */
+  srandom ((unsigned) time(NULL));
+
+  file_to_open = myfiles[get_random(N_FILES)];
+  fd  = open (file_to_open, O_RDONLY);
+  flock(fd, LOCK_SH);
+  
+  printf("Lock acquired by process %d\n", getpid());
+
+  printf("Monitor will check if file %s is consistent...\n", file_to_open);
+
+  if (fd == -1) {
+    perror ("Error opening file");
+    exit (-1);
+  }
+  else {
+    char string_to_read[STR_LENGTH];
+    char first_string[STR_LENGTH];
+    int  i;
+
+    if (read (fd, first_string, STR_LENGTH) == -1) {
+      perror ("Error reading file");
+      exit (-1);
+    }
+    for (i=0; i<TOTAL_LINES-1; i++) {
+      
+      if (read (fd, string_to_read, STR_LENGTH) == -1) {
+		perror ("Error reading file");
+		exit (-1);
+      }
+
+      if (strncmp(string_to_read, first_string, STR_LENGTH)) {
+		fprintf (stderr, "Inconsistent file: %s\n", file_to_open);
+		exit (-1);
+      }
+    }
+	sleep(10);
+    
+    
+    flock(fd, LOCK_UN);
+    
+    printf("Lock released by process %d\n", getpid());
+     
+    if (close (fd) == -1)  {
+      perror ("Error closing file");
+      exit (-1);
+    }
+  }
+  printf("YES.\n");
 }
+
+
