61e8f996a711a64b86de9407df0444826b29c277
Pedro Saraiva <pedrovskyandre@hotmail.com>
2014-10-30 18:23:40 +0000

    Update consts.h
    ex3

--- consts.h
+++ consts.h
@@ -4,7 +4,7 @@
  * Pedro Saraiva nº70848
  * Francisco Dias nº75328
  * 
- * Exercício 1
+ * Exercício 3
  * */
 
 #define TOTAL_WRITES 5120
@@ -12,6 +12,8 @@
 #define N_STRINGS 10
 #define STR_LENGTH 10
 #define N_FILES 5
+#define N_CHILDREN 10
+#define get_random(max) rand()%max
 
 const char *str[N_STRINGS];
 
